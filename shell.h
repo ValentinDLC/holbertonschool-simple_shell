8331eb122b323112ae7871b700cf07e0cdea33b2
ValentinDLC <valentin.dardenne9@gmail.com>
2025-08-13 22:11:04 +0200

    update with environment variable

--- shell.h
+++ shell.h
@@ -7,8 +7,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #define MAX_CMD_LEN 1024
+
+/* variable for environment */
+extern char **environ;
 
 /* Displays the shell prompt */
 void print_prompt(void);
@@ -18,5 +22,8 @@ int read_command(char *buffer, size_t size);
 
 /* Executes a command */
 int execute_command(char *command, char **env);
+
+/* Check if a file exists and is executable */
+int is_excecutable(char *path);
 
 #endif
