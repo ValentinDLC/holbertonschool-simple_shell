eac11c20ea25b2c90f019a2afbc0dc50dd4b9a18
ValentinDLC <valentin.dardenne9@gmail.com>
2025-08-19 16:36:23 +0200

    Add new function prototypes and organize them source file

--- shell.h
+++ shell.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <ctype.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -16,19 +15,25 @@
 /* variable for environment */
 extern char **environ;
 
-/* Displays the shell prompt */
-void print_prompt(void);
+/* prompt.c */
+void print_prompt(void); /* Displays the shell prompt */
 
-/* Reads a command from stdin */
-int read_command(char *buffer, size_t size);
+/* input.c */
+int read_command(char *buffer, size_t size); /* Reads a command from stdin */
+int is_space(char c); /* Check if character is space */
 
-/* Executes a command */
-int execute_command(char *command, char **env);
+/* executor.c */
+int execute_command(char *command, char **env); /* Executes a command */
+void free_path_if_needed(char *cmd_path, char *original); /* Frees path if it was dynamically allocated */
 
-/* Check if a file exists and is executable */
-int is_excecutable(char *path);
+/* path.c */
+int is_excecutable(char *path); /* Check if a file exists and is executable */
+char *find_command(char *command); /* Find command in PATH */
+char *search_in_dir(char *dir, char *command); /* Searches for a command in a directory */
 
-/* Parses a command into arguments */
-int parse_command(char *command, char **args);
+
+/* Parser.c */
+int parse_command(char *command, char **args); /* Parses a command into arguments */
+char *get_next_arg(char **start); /* Returns the next argument from a command string */
 
 #endif
