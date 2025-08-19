dcf891946cca972a89c4798d7ad3757e981c5b75
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-19 11:11:14 +0200

    add loops for display and read input

--- simple_shell.c
+++ simple_shell.c
@@ -1,1 +1,33 @@
 #include "shell.h"
+/**
+ * main - Entry point for the simple shell
+ *
+ * @ac: Argument count
+ * @av: Argument vector
+ * @env: Environment variables
+ *
+ * Return:
+ */
+int main(int ac, char **av, char **env)
+{
+    (void)ac;
+    (void)av;
+    /*Loops*/
+    while (true)
+    {
+        /*Display prompt*/
+        display_prompt();
+        /*Read input*/
+        if (read_input() == -1)
+        {
+            break;
+        }
+        
+    }
+    
+    /*Parse Command*/
+    /*Execute command*/
+    /*Wait for completion*/
+    /*Repeat/EOF*/
+    return(0);
+}
