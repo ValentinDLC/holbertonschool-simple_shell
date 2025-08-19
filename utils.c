20f94dd6e8201c22634f80148dab90990f8c1850
ValentinDLC <valentin.dardenne9@gmail.com>
2025-08-19 17:08:47 +0200

    replace function in another file

---
+++ utils.c
@@ -0,0 +1,17 @@
+/**
+ * is_empty_or_spaces - Checks if a string is empty or contains only spaces.
+ * @str: The string to check.
+ *
+ * Return: 1 if the string is empty or contains only spaces,
+ *         0 otherwise.
+ */
+int is_empty_or_spaces(char *str)
+{
+	while (*str)
+	{
+		if (!is_space(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
