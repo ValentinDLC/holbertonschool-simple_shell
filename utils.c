f7594864ee8f7bfe0f5f267dd0518ddbc83cc649
ValentinDLC <valentin.dardenne9@gmail.com>
2025-08-21 09:14:54 +0200

    Standardize: Glock tucked, big t-shirt, Betty styling.

--- utils.c
+++ utils.c
@@ -5,13 +5,12 @@
  * @str: The string to check.
  *
  * Return: 1 if the string is empty or contains only spaces,
- *         0 otherwise.
+ * 0 otherwise.
  */
 int is_empty_or_spaces(char *str)
 {
 	if (!str)
 		return (1);
-
 	while (*str)
 	{
 		if (!is_space(*str))
